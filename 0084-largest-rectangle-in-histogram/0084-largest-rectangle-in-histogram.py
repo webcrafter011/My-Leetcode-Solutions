33c8171690a7688c70f16761ac50495fa8e91eb2
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-08-27 03:29:28 +0000

    Time: 147 ms (80.55%), Space: 29.3 MB (84.5%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -2,27 +2,49 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
 
-        # Next Smaller Elements
-        NSE = [n] * n
-        st = []
-        for i in range(n - 1, -1, -1):
-            while st and heights[st[-1]] >= heights[i]:
-                st.pop()
-            NSE[i] = st[-1] if st else n
-            st.append(i)
+        # # Next Smaller Elements
+        # NSE = [n] * n
+        # st = []
+        # for i in range(n - 1, -1, -1):
+        #     while st and heights[st[-1]] >= heights[i]:
+        #         st.pop()
+        #     NSE[i] = st[-1] if st else n
+        #     st.append(i)
         
-        # Previous Smaller Elements
-        PSE = [-1] * n
+        # # Previous Smaller Elements
+        # PSE = [-1] * n
+        # st = []
+        # for i in range(n):
+        #     while st and heights[st[-1]] > heights[i]:
+        #         st.pop()
+        #     PSE[i] = st[-1] if st else -1
+        #     st.append(i)
+
+        # max_area = float('-inf')
+        # for i in range(n):
+        #     area = heights[i] * (NSE[i] - PSE[i] - 1)
+        #     max_area = max(max_area, area)
+        
+        # return max_area
+
+        # Most optimal approach
+        maxArea = float('-inf')
         st = []
-        for i in range(n):
+        for i in range(len(heights)):
             while st and heights[st[-1]] > heights[i]:
+                element = st[-1]
                 st.pop()
-            PSE[i] = st[-1] if st else -1
+                nse = i
+                pse = st[-1] if st else -1
+                maxArea = max(maxArea, (nse - pse - 1) * heights[element])
+            
             st.append(i)
 
-        max_area = float('-inf')
-        for i in range(n):
-            area = heights[i] * (NSE[i] - PSE[i] - 1)
-            max_area = max(max_area, area)
+        while st:
+            nse = n
+            element = st[-1]
+            st.pop()
+            pse = st[-1] if st else -1
+            maxArea = max(maxArea, (nse - pse - 1) * heights[element])
         
-        return max_area
+        return maxArea
