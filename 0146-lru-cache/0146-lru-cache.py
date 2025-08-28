7b61a6737b8715042c2b3a04cfa175a5f0fb22a1
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-08-28 17:02:28 +0000

    Time: 161 ms (32.25%), Space: 78 MB (72.8%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,47 +1,46 @@
 class Node:
-    def __init__(self, key: int, value: int):
+    def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.prev = None
         self.next = None
-
+        self.prev = None
 
 class LRUCache:
     def __init__(self, capacity: int):
+        self.cache = {} # key -> Node 
         self.capacity = capacity
-        self.cache = {}  # key → Node
 
-        # Dummy head & tail to avoid edge cases
-        self.head = Node(0, 0)   # left = MRU
-        self.tail = Node(0, 0)   # right = LRU
+        # dummy head and tail 
+        self.head = Node(0, 0) # left - Most Recently Used
+        self.tail = Node(0, 0) # right - Least Recently Used
         self.head.next = self.tail
         self.tail.prev = self.head
-
-    # helper: remove a node from DLL
-    def _remove(self, node: Node):
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
-
-    # helper: insert node right after head (MRU position)
-    def _insert(self, node: Node):
+    
+    # helper function to insert in DLL
+    def _insert(self, node):
         node.next = self.head.next
         node.prev = self.head
-        self.head.next.prev = node
+        node.next.prev = node
         self.head.next = node
+    
+    # helper functoin to remove node
+    def _remove(self, node):
+        prv, nxt = node.prev, node.next
+        prv.next = nxt
+        nxt.prev = prv
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._insert(node)
+            return node.value
+        else:
             return -1
-        node = self.cache[key]
-        # move node to MRU position
-        self._remove(node)
-        self._insert(node)
-        return node.value
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            # remove old node
+            # remove already existing node
             self._remove(self.cache[key])
 
         node = Node(key, value)
@@ -49,7 +48,6 @@ class LRUCache:
         self._insert(node)
 
         if len(self.cache) > self.capacity:
-            # evict LRU (node before tail)
             lru = self.tail.prev
             self._remove(lru)
             del self.cache[lru.key]
