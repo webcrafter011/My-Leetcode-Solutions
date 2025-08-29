95fde955eaa3a427a0d56942548ecd0a57e09dc2
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-08-29 13:01:43 +0000

    Time: 105 ms (8.35%), Space: 20.5 MB (10.66%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        # helper function to check if the particular subarray has at max k zeroes
+        # def subArrWithMaxKZeroes(arr):
+        #     count = 0
+        #     for num in arr:
+        #         if num == 0:
+        #             count += 1
+            
+        #     return count <= k
+        
+        # maxLen = 0
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i, n):
+        #         arr = nums[i:j + 1]
+        #         if subArrWithMaxKZeroes(arr):
+        #             maxLen = max(maxLen, len(arr))
+            
+        # return maxLen
+
+        l = r = 0
+        maxLen = 0
+        zeroes = 0
+
+        while r < len(nums):
+            if nums[r] == 0:
+                zeroes += 1
+            
+            while zeroes > k:
+                if nums[l] == 0:
+                    zeroes -= 1
+                l += 1
+            
+            if zeroes <= k:
+                currLen = r - l + 1
+                maxLen = max(maxLen, currLen)
+            r += 1
+        
+        return maxLen
+
