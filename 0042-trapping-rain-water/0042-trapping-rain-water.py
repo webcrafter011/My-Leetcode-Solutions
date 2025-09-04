6aec36d13f35222d7537425a26c1c8446fddce50
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-09-04 17:26:37 +0000

    Time: 3 ms (97.28%), Space: 19.2 MB (93.32%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -2,18 +2,38 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
 
-        suf_max = [None] * n
-        suf_max[-1] = height[-1]
-        for i in range(n - 2, -1, -1):
-            suf_max[i] = max(suf_max[i + 1], height[i])
+        # suf_max = [None] * n
+        # suf_max[-1] = height[-1]
+        # for i in range(n - 2, -1, -1):
+        #     suf_max[i] = max(suf_max[i + 1], height[i])
         
-        left_max = float('-inf')
+        # left_max = float('-inf')
+        # trapped_water = 0
+        # for i in range(n):
+        #     left_max = max(left_max, height[i])
+        #     right_max = suf_max[i]
+        #     if height[i] < left_max and height[i] < right_max:
+        #         trapped_water += min(left_max, right_max) - height[i]
+
+        # Optimal solution two pointer 
+        l = 0
+        r = n - 1
+        lmax = rmax = 0
         trapped_water = 0
-        for i in range(n):
-            left_max = max(left_max, height[i])
-            right_max = suf_max[i]
-            if height[i] < left_max and height[i] < right_max:
-                trapped_water += min(left_max, right_max) - height[i]
+
+        while l < r:
+            if height[l] < height[r]:
+                if lmax > height[l]:
+                    trapped_water += lmax - height[l]
+                else:
+                    lmax = height[l]
+                l += 1
+            else:
+                if rmax > height[r]:
+                    trapped_water += rmax - height[r]
+                else:
+                    rmax = height[r]
+                r -= 1
             
         
         return trapped_water
