a8a30e3772b7ad793a5f3cc529b9782d6e8676a7
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-09-06 10:00:17 +0000

    Time: 157 ms (58.37%), Space: 30.4 MB (56.09%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,50 +1,26 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        maxArea = 0
+        st = []
         n = len(heights)
 
-        # # Next Smaller Elements
-        # NSE = [n] * n
-        # st = []
-        # for i in range(n - 1, -1, -1):
-        #     while st and heights[st[-1]] >= heights[i]:
-        #         st.pop()
-        #     NSE[i] = st[-1] if st else n
-        #     st.append(i)
-        
-        # # Previous Smaller Elements
-        # PSE = [-1] * n
-        # st = []
-        # for i in range(n):
-        #     while st and heights[st[-1]] > heights[i]:
-        #         st.pop()
-        #     PSE[i] = st[-1] if st else -1
-        #     st.append(i)
-
-        # max_area = float('-inf')
-        # for i in range(n):
-        #     area = heights[i] * (NSE[i] - PSE[i] - 1)
-        #     max_area = max(max_area, area)
-        
-        # return max_area
-
-        # Most optimal approach
-        maxArea = float('-inf')
-        st = []
-        for i in range(len(heights)):
+        for i in range(n):
             while st and heights[st[-1]] > heights[i]:
-                element = st[-1]
-                st.pop()
+                el = st[-1]
                 nse = i
+                st.pop()
                 pse = st[-1] if st else -1
-                maxArea = max(maxArea, (nse - pse - 1) * heights[element])
+                area = (nse - pse - 1) * heights[el]
+                maxArea = max(maxArea, area)
             
             st.append(i)
-
+        
         while st:
+            el = st[-1]
             nse = n
-            element = st[-1]
             st.pop()
             pse = st[-1] if st else -1
-            maxArea = max(maxArea, (nse - pse - 1) * heights[element])
-        
+            area = (nse - pse - 1) * heights[el]
+            maxArea = max(maxArea, area)
+
         return maxArea
