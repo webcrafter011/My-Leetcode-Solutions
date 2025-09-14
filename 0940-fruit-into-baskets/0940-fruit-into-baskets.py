eca779fe980eddd4c36a2cbd2356f33781a61f23
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-09-14 23:54:23 +0530

    Time: 215 ms (61.58%), Space: 17.2 MB (18.21%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -1,36 +1,22 @@
-class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
-
-        # maxCount = 0
-        # n = len(fruits)
-
-        # for i in range(n):
-        #     basket = {}
-        #     for j in range(i, n):
-        #         fruit = fruits[j]
-        #         basket[fruit] = basket.get(fruit, 0) + 1
-
-        #         if len(basket) > 2:
-        #             break
-                
-        #         maxCount = max(maxCount, j - i + 1)
-
-        # return maxCount
-        
-        l = r = 0
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        left = 0
+        baskets = {}
         maxLen = 0
-        h = {}
 
-        while r < len(fruits):
-            h[fruits[r]] = h.get(fruits[r], 0) + 1
+        for right in range(len(fruits)):
+            baskets[fruits[right]] = baskets.get(fruits[right], 0) + 1
 
-            if len(h) > 2:
-                h[fruits[l]] -= 1
-                if h[fruits[l]] == 0:
-                    del h[fruits[l]]
-                l += 1
-
-            maxLen = max(maxLen, r - l + 1)
-            r += 1
-
+            if len(baskets) > 2:
+                baskets[fruits[left]] -= 1
+                if baskets[fruits[left]] == 0:
+                    del baskets[fruits[left]]
+                left += 1
+            
+            maxLen = max(maxLen, right - left + 1)
+        
         return maxLen
