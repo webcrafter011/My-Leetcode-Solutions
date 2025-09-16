d49fd11cbe2a208c816316c4e75de6b2131feb22
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-09-16 20:24:57 +0530

    Time: 36 ms (24.27%), Space: 18.5 MB (99.02%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,35 +1,22 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        # sum_map = {0:1}
-        # pre_sum = 0
-        # count = 0
 
-        # for num in nums:
-        #     pre_sum += num
+        def getMax(goal):
+            if goal < 0:
+                return 0
 
-        #     if pre_sum - goal in sum_map:
-        #         count += sum_map[pre_sum - goal]
+            left = count = currSum = 0
+
+            for right in range(len(nums)):
+                currSum += nums[right]
+
+                while currSum > goal:
+                    currSum -= nums[left]
+                    left += 1
+                
+                count += (right - left + 1)
             
-        #     sum_map[pre_sum] = sum_map.get(pre_sum, 0) + 1
-        
-        # return count
-
-        def atMax(k):
-            l = 0
-            r = 0
-            count = 0
-            curr_sum = 0
-
-            while r < len(nums):
-                curr_sum += nums[r]
-
-                while curr_sum > k and l <= r:
-                    curr_sum -= nums[l]
-                    l += 1
-                
-                count += r - l + 1
-                r += 1
-                
             return count
+        
+        return getMax(goal) - getMax(goal - 1)
 
-        return atMax(goal) - atMax(goal - 1)
