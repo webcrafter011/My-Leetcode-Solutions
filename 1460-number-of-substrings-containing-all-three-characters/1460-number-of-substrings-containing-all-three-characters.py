90efbea6a10c666c3f2cd892525f3997626de731
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-09-16 22:39:16 +0530

    Time: 119 ms (31.49%), Space: 17.8 MB (64.28%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,17 +1,18 @@
 class Solution:
-    def numberOfSubstrings(self, s: str) -> int:
-        h = {'a': 0, 'b': 0, 'c': 0}
-        r = l = 0
-        count = 0
+    def numberOfSubstrings(self, s: str) -> int:  
+        h = {
+            'a': 0,
+            'b': 0,
+            'c': 0
+        }
+        left = count = 0
 
-        while r < len(s):
-            h[s[r]] = h.get(s[r], 0) + 1
+        for right in range(len(s)):
+            h[s[right]] = h.get(s[right], 0) + 1
 
             while h['a'] > 0 and h['b'] > 0 and h['c'] > 0:
-                h[s[l]] -= 1
-                count += len(s) - r
-                l += 1
-
-            r += 1
-        
+                h[s[left]] -= 1
+                count += len(s) - right
+                left += 1
+            
         return count
