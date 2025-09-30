96bec81a309881d142210f661e8e3f84c39ef865
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-09-30 22:22:20 +0530

    Time: 35 ms (56.6%), Space: 19.2 MB (45.19%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,15 +1,23 @@
 import heapq
+from collections import Counter
 
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize:
             return False
+        
         count = Counter(hand)
-        for x in sorted(count):
-            while count[x] > 0:
-                need = count[x]
-                for i in range(groupSize):
-                    if count[x + i] < need:
+        heap = list(count.keys())
+        heapq.heapify(heap)
+        while heap:
+            first = heap[0]
+            
+            for i in range(first, first + groupSize):
+                if i not in count:
+                    return False
+                count[i] -= 1
+                if count[i] == 0:
+                    if i != heap[0]:
                         return False
-                    count[x + i] -= need
+                    heapq.heappop(heap)
         return True
