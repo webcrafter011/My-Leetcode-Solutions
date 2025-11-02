4b88dcec7cbed4651b3b2b5a376745bfbf147bfd
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-11-02 16:49:32 +0530

    Time: 4 ms (15.33%), Space: 21 MB (72.62%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -9,20 +9,47 @@ class Solution:
         self.result = None
         self.rank = 0
 
-        def inorder(node):
-            if not node:
-                return
-            if self.result is not None:
-                return
+        # def inorder(node):
+        #     if not node:
+        #         return
+        #     if self.result is not None:
+        #         return
             
-            inorder(node.left)
+        #     inorder(node.left)
 
-            self.rank += 1
-            if self.result is None and self.rank == k:
-                self.result = node.val
-                return
+        #     self.rank += 1
+        #     if self.result is None and self.rank == k:
+        #         self.result = node.val
+        #         return
         
-            inorder(node.right)
+        #     inorder(node.right)
         
-        inorder(root)
+        # inorder(root)
+        # return self.result
+        
+        # trial with morris traversal 
+        node = root
+
+        while node:
+            if not node.left:
+                self.rank += 1
+                if self.rank == k:
+                    self.result = node.val
+                    break
+                node = node.right
+            else:
+                prev = node.left
+                while prev.right and prev.right != node:
+                    prev = prev.right
+                if not prev.right:
+                    prev.right = node
+                    node = node.left
+                elif prev.right:
+                    prev.right = None
+                    self.rank += 1
+                    if self.rank == k:
+                        self.result = node.val
+                        break
+                    node = node.right
+        
         return self.result
