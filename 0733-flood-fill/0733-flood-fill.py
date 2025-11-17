9f4c01250677db734442231a402ff60b28ffb353
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-11-17 19:46:45 +0530

    Time: 3 ms (17.18%), Space: 17.9 MB (88.7%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,18 +2,17 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         if color == image[sr][sc]:
             return image
-
-        rows, cols = len(image), len(image[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        q = deque([(sr, sc)])
         og_color = image[sr][sc]
+        rows, cols = len(image), len(image[0])
+        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+        q = deque([(sr, sc)])
 
         while q:
-            pr, pc = q.popleft()
-            image[pr][pc] = color
+            r, c = q.popleft()
+            image[r][c] = color
             for dr, dc in directions:
-                row, col = dr + pr, dc + pc
-                if 0 <= row < rows and 0 <= col < cols and image[row][col] == og_color:
-                    q.append((row, col))
-
+                nr, nc = dr + r, dc + c
+                if 0 <= nr < rows and 0 <= nc < cols and image[nr][nc] == og_color:
+                    q.append((nr, nc))
+        
         return image
