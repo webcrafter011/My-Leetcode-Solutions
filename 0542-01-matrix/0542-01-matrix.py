29dd448740ba267c3cb09ec3307608666fe14615
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-11-19 20:40:11 +0530

    Time: 102 ms (92.38%), Space: 20.5 MB (63.6%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,24 +1,21 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        m, n = len(mat), len(mat[0])
-        res = [[-1] * n for _ in range(m)]
+        rows, cols = len(mat), len(mat[0])
+        res = [[-1] * cols for _ in range(rows)]
+        q = deque([])
 
-        q = deque()
-        for r in range(m):
-            for c in range(n):
-                if mat[r][c] == 0:
-                    res[r][c] = 0
-                    q.append((r, c))
+        for i in range(rows):
+            for j in range(cols):
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                    res[i][j] = 0
         
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
         while q:
-            r, c = q.popleft()
-            for dr, dc in directions:
-                row = r + dr
-                col = c + dc
-                if 0 <= row < m and 0 <= col < n and res[row][col] == -1:
-                    res[row][col] = res[r][c] + 1
-                    q.append((row, col))
-
+            row, col = q.popleft()
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr, nc = dr + row, dc + col
+                if 0 <= nr < rows and 0 <= nc < cols and res[nr][nc] == -1:
+                    res[nr][nc] = res[row][col] + 1
+                    q.append((nr, nc))
+        
         return res
