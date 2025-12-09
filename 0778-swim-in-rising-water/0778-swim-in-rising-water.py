a610f2deb474142b57f74ef284aa4868f45a52c7
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-12-09 20:02:43 +0530

    Time: 31 ms (55.03%), Space: 18.6 MB (25.2%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -0,0 +1,71 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = [0] * (n + 1)
+        for i in range(len(self.parent)):
+            self.parent[i] = i
+        self.rank = [0] * (n + 1)
+        self.size = [1] * (n + 1)
+    
+    def find_par(self, node):
+        if node == self.parent[node]:
+            return node
+
+        self.parent[node] = self.find_par(self.parent[node])
+        return self.parent[node]
+    
+    def union_by_rank(self, u, v):
+        ultp_u = self.find_par(u)
+        ultp_v = self.find_par(v)
+        
+        if ultp_u == ultp_v:
+            return
+        if self.rank[ultp_u] < self.rank[ultp_v]:
+            self.parent[ultp_u] = ultp_v
+        elif self.rank[ultp_u] > self.rank[ultp_v]:
+            self.parent[ultp_v] = ultp_u
+        else:
+            self.parent[ultp_v] = ultp_u
+            self.rank[ultp_u] += 1
+    
+    def union_by_size(self, u, v):
+        ultp_u = self.find_par(u)
+        ultp_v = self.find_par(v)
+        
+        if ultp_u == ultp_v:
+            return
+        if self.size[ultp_u] < self.size[ultp_v]:
+            self.parent[ultp_u] = ultp_v
+            self.size[ultp_v] += self.size[ultp_u]
+        else:
+            self.parent[ultp_v] = ultp_u
+            self.size[ultp_u] += self.size[ultp_v]
+        
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        dsu = DisjointSet(n*n)
+        cells = []
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        open_cells = [[False] * n for _ in range(n)]
+
+        def index(r, c):
+            return r * n + c
+
+        for r in range(n):
+            for c in range(n):
+                cells.append((grid[r][c], r, c))
+        
+        cells.sort(key = lambda x: x[0])
+
+        for h, r, c in cells:
+            open_cells[r][c] = True
+            for dr, dc in directions:
+                nr, nc = dr + r, dc + c
+                if 0 <= nr < n and 0 <= nc < n and open_cells[nr][nc]:
+                    dsu.union_by_rank(index(r, c), index(nr, nc))
+            
+            if dsu.find_par(0) == dsu.find_par(index(n - 1, n - 1)):
+                return h
+
+
+
