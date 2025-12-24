9fcbb5b540cd6ca5718ca0b67c41e667c700a9ad
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-12-24 13:52:24 +0530

    Time: 1232 ms (11.6%), Space: 21.1 MB (24.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,43 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [-1] * (amount + 1)
+        n = len(coins)
+        dp = [[float('inf')] * (amount + 1) for _ in range(n)]
 
-        def solve(total):
-            if total > amount:
-                return float('inf')
-            if total == amount:
-                return 0
-            if dp[total] != -1:
-                return dp[total]
-                
-            mini = float('inf')
-            for coin in coins:
-                mini = min(mini, 1 + solve(total + coin))
+        # def solve(i, target):
+        #     if target == 0:
+        #         return 0 
 
-            dp[total] = mini
-            return mini
+        #     if i == 0:
+        #         if target % coins[i] == 0:
+        #             return target // coins[i]
+        #         else:
+        #             return float('inf')
+            
+        #     if dp[i][target] != -1:
+        #         return dp[i][target]
 
-        ans = solve(0)
+            # not_take = solve(i - 1, target)
+            # take = float('inf')
+            # if coins[i] <= target:
+            #     take = 1 + solve(i, target - coins[i])
+            # dp[i][target] = min(take, not_take)
+        #     return dp[i][target]
+
+        # Tabulation:
+
+        for target in range(amount + 1):
+            if target % coins[0] == 0:
+                dp[0][target] = target // coins[0]
+            
+        for i in range(1, n):
+            for target in range(amount + 1):
+                not_take = dp[i - 1][target]
+                take = float('inf')
+                if coins[i] <= target:
+                    take = 1 + dp[i][target - coins[i]]
+                dp[i][target] = min(take, not_take)
+
+
+            
+        ans = dp[n - 1][amount]
         return ans if ans != float('inf') else -1
