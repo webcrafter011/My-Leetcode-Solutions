7f397290e16f7494c15c3f64681b97d5ec939601
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-12-24 21:56:20 +0530

    Time: 362 ms (54.02%), Space: 49.7 MB (48.52%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,26 @@
+from functools import cache
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
-
+        dp = [[-1] * (amount + 1) for _ in range(n)]
+        
         def solve(i, target):
             if i == 0:
                 if target % coins[0] == 0:
                     return 1
                 return 0
             
+            if dp[i][target] != -1:
+                return dp[i][target]
+
             not_take = solve(i - 1, target)
             take = 0
             if coins[i] <= target:
                 take = solve(i, target - coins[i])
             
-            return take + not_take
+            dp[i][target] = take + not_take
+            return dp[i][target]
         
 
         return solve(n - 1, amount)
