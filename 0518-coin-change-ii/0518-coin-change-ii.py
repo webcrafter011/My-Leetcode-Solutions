4b29594d6509ddadddc28a5536523aeb9ee71c1a
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-12-24 22:00:19 +0530

    Time: 516 ms (39.5%), Space: 64.8 MB (26.91%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -3,26 +3,41 @@ from functools import cache
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
-        dp = [[-1] * (amount + 1) for _ in range(n)]
+        dp = [[0] * (amount + 1) for _ in range(n)]
         
-        def solve(i, target):
-            if i == 0:
-                if target % coins[0] == 0:
-                    return 1
-                return 0
+        # Top-Down solution written by myself
+        # def solve(i, target):
+        #     if i == 0:
+        #         if target % coins[0] == 0:
+        #             return 1
+        #         return 0
             
-            if dp[i][target] != -1:
-                return dp[i][target]
+        #     if dp[i][target] != -1:
+        #         return dp[i][target]
 
-            not_take = solve(i - 1, target)
-            take = 0
-            if coins[i] <= target:
-                take = solve(i, target - coins[i])
+        #     not_take = solve(i - 1, target)
+        #     take = 0
+        #     if coins[i] <= target:
+        #         take = solve(i, target - coins[i])
             
-            dp[i][target] = take + not_take
-            return dp[i][target]
+        #     dp[i][target] = take + not_take
+        #     return dp[i][target]
         
+        # Tabulation
+        for target in range(amount + 1):
+            if target % coins[0] == 0:
+                dp[0][target] = 1
+        
+        for i in range(1, n):
+            for target in range(amount + 1):
+                not_take = dp[i - 1][target]
+                take = 0
+                if coins[i] <= target:
+                    take = dp[i][target - coins[i]]
+                
+                dp[i][target] = take + not_take
+                
 
-        return solve(n - 1, amount)
+        return dp[n - 1][amount]
 
             
