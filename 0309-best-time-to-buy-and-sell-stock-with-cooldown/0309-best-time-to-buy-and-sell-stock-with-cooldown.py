c0d958cf588f8ad043f7cd28f8ac6b6a4d786204
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2025-12-31 18:18:30 +0530

    Time: 4 ms (53.71%), Space: 18 MB (83.96%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[-1] * 2 for _ in range(n)]
+
+        def solve(i, buy):
+            if i >= n:
+                return 0
+
+            if dp[i][buy] != -1:
+                return dp[i][buy]
+
+            profit = 0
+            if buy:
+                profit = max(
+                    -prices[i] + solve(i + 1, 0),
+                    solve(i + 1, 1)
+                )
+            else:
+                profit = max(
+                    prices[i] + solve(i + 2, 1),
+                    solve(i + 1, 0)
+                )
+            
+            dp[i][buy] = profit
+            return dp[i][buy]
+            
+        return solve(0, 1)
