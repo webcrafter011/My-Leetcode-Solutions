8259b8572789f190196f55827af4d588e44f333a
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-01-02 12:30:09 +0530

    Time: 0 ms (100%), Space: 18.5 MB (95.44%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        seen = set()
-
-        for num in nums:
-            if num in seen:
-                return num
-            seen.add(num)
+        
+        for i in range(len(nums) - 2):
+            if nums[i] == nums[i + 1] or nums[i] == nums[i + 2]:
+                return nums[i]
+        
+        return nums[-1]
