b75ba593e8badcee9d7988e99f679a6f27f39ead
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-01-10 10:49:40 +0530

    Time: 167 ms (99.25%), Space: 20.2 MB (73.27%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -8,19 +8,30 @@ class Solution:
 
         total = find_ascii_value(s1) + find_ascii_value(s2)
         n, m = len(s1), len(s2)
-        dp = [[-1] * m for _ in range(n)]
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
         
-        def solve(i, j):
-            if i < 0 or j < 0:
-                return 0
+        # def solve(i, j):
+        #     if i < 0 or j < 0:
+        #         return 0
             
-            if dp[i][j] != -1:
-                return dp[i][j]
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
 
-            if s1[i] == s2[j]:
-                return ord(s1[i]) + solve(i - 1, j - 1)
+        #     if s1[i] == s2[j]:
+        #         return ord(s1[i]) + solve(i - 1, j - 1)
             
-            dp[i][j] = max(solve(i - 1, j), solve(i, j - 1))
-            return dp[i][j] 
+        #     dp[i][j] = max(solve(i - 1, j), solve(i, j - 1))
+        #     return dp[i][j] 
         
-        return total - 2 * solve(n - 1, m - 1)
+        # return total - 2 * solve(n - 1, m - 1)
+
+        # Tabulation Approach
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = ord(s1[i - 1]) + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return total - 2 * dp[n][m]
