56fe2a47c8da03658203c11e89d52fb6982e34b3
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-01-11 12:37:38 +0530

    Time: 31 ms (86.62%), Space: 24.8 MB (5.85%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -4,6 +4,7 @@ class Solution:
         m = len(matrix[0])
         preSum = [[0] * m for _ in range(n)]
 
+        # calculating the presum map to recreate the histogram structure
         for j in range(m):
             for i in range(n):
                 if matrix[i][j] == '1':
@@ -11,26 +12,30 @@ class Solution:
                 else:
                     preSum[i][j] = 0
         
+        # calculating max histogram area by taking each row as x-axis
+
         maxArea = 0
-        for heights in preSum:
+        for row in preSum:
             st = []
             for i in range(m):
-                while st and heights[st[-1]] > heights[i]:
-                    el = st[-1]
+                while st and row[st[-1]] > row[i]:
+                    element = st[-1]
                     nse = i
                     st.pop()
                     pse = st[-1] if st else -1
-                    area = (nse - pse - 1) * heights[el]
+                    area = row[element] * (nse - pse - 1)
                     maxArea = max(maxArea, area)
-                st.append(i)
                 
+                # append current index in stack
+                st.append(i)
+            
             while st:
-                el = st[-1]
+                element = st[-1]
                 nse = m
                 st.pop()
                 pse = st[-1] if st else -1
-                area = (nse - pse - 1) * heights[el]
+                area = row[element] * (nse - pse - 1)
                 maxArea = max(maxArea, area)
-                
+            
         return maxArea
 
