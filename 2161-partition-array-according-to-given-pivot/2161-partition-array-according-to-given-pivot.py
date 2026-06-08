cdd8cc05177f370b8d07f7fd0879d1808cdc2e28
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-06-08 08:51:14 +0530

    Time: 69 ms (13%), Space: 34 MB (17.69%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,35 +1,21 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        smaller = 0
-        equal = 0
-        larger = 0
+        n = len(nums)
+        ans = [0] * n
 
-        for num in nums:
-            if num < pivot:
-                smaller += 1
-            elif num > pivot:
-                larger += 1
-            else:
-                print(f'num={num}, pivot={pivot}')
-                equal += 1
+        less = 0
+        greator = n - 1
+
+        for i, j in zip(range(n), range(n - 1, -1, -1)):
+            if nums[i] < pivot:
+                ans[less] = nums[i]
+                less += 1
+            if nums[j] > pivot:
+                ans[greator] = nums[j]
+                greator -= 1
         
-        first_smaller = 0
-        first_equal = smaller
-        first_larger = smaller + equal
-
-        print(first_smaller, first_equal, first_larger)
-
-        ans = [-1] * len(nums)
-
-        for num in nums:
-            if num < pivot:
-                ans[first_smaller] = num
-                first_smaller += 1
-            elif num > pivot:
-                ans[first_larger] = num
-                first_larger += 1
-            else:
-                ans[first_equal] = num
-                first_equal += 1
-        
+        while less <= greator:
+            ans[less] = pivot
+            less += 1
+    
         return ans
