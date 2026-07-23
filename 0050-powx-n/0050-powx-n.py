e760fd67c29415ca4cf7578ee2095e83843214fc
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-07-23 19:38:08 +0530

    Time: 2 ms (20.23%), Space: 19.6 MB (18.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,17 +1,33 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        nn = abs(n)
-        ans = 1
+        # nn = abs(n)
+        # ans = 1
         
-        while nn:
-            # for odd power
-            if nn & 1:
-                ans *= x
-                nn -= 1
-            else:
-                nn //= 2
-                x *= x
+        # while nn:
+        #     # for odd power
+        #     if nn & 1:
+        #         ans *= x
+        #         nn -= 1
+        #     else:
+        #         nn //= 2
+        #         x *= x
         
+        # if n < 0:
+        #     return 1/ans
+        # return ans
+        
+
+        def calc(base: float, power: int, ans: float):
+            if power == 0:
+                return ans
+            
+            # odd power
+            if power & 1:
+                return calc(base, power - 1, ans * base)
+            
+            return calc(base * base, power // 2, ans)
+
+        ans = calc(x, abs(n), 1)
         if n < 0:
             return 1/ans
         return ans
