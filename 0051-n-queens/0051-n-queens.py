9b29574a9836ed397b9e9e3e40ec5473877f4f2a
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-07-30 19:12:26 +0530

    Time: 11 ms (66.17%), Space: 19.7 MB (68.87%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,48 +1,46 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        # helper funtion
-        def isSafe1(row, col, board):
-            r = row
-            c = col
+        board = [['.'] * n for _ in range(n)]
+        res = []
+        cols = set()
+        diag1 = set()   # row - col
+        diag2 = set()   # row + col
 
-            # check anti clock diagonal
-            while r >= 0 and c >= 0:
-                if board[r][c] == 'Q':
-                    return False
-                r -= 1
-                c -= 1
-            
-            r = row
-            c = col
-            # check left side
-            while c >= 0:
-                if board[r][c] == 'Q':
-                    return False
-                c -= 1
-            
-            r = row
-            c = col
-            # check clockwise diagonal
-            while r < n and c >= 0:
-                if board[r][c] == 'Q':
-                    return False
-                r += 1
-                c -= 1
-            
-            return True
+        def add_result(res, board):
+            copy = []
+            for row in board:
+                cur = ''.join(row)
+                copy.append(cur)
+            res.append(copy)
 
-        # backtracking function
-        def backtrack(col, board, ans):
-            if col == n:
-                ans.append(board.copy())
-                return
+        def build(row, board):
+            if row == n:
+                add_result(res, board)
+                return 
             
-            for row in range(n):
-                if isSafe1(row, col, board):
-                    board[row] = board[row][:col] + 'Q' + board[row][col + 1:]
-                    backtrack(col + 1, board, ans)
-                    board[row] = board[row][:col] + '.' + board[row][col + 1:]
+            for col in range(n):
+                if (
+                    col in cols 
+                    or row - col in diag1 
+                    or row + col in diag2
+                ):
+                    continue
+                
+                board[row][col] = 'Q'
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+                
+                # explore next row
+                build(row + 1, board)
+
+                # backtrack by removing all the values in sets and existing queen
+                board[row][col] = '.'
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
             
-        ans = []
-        backtrack(0, ['.'*n for _ in range(n)], ans)
-        return ans
+        build(0, board)
+        return res
+
+                
