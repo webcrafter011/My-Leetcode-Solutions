8ce94cb75f406271fb1c25a09dcc34d6bf957d38
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-08-19 10:24:10 +0530

    Time: 70 ms (32.13%), Space: 23.5 MB (12.85%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        res_rows = defaultdict(set)
+
+        for row, seat in reservedSeats:
+            res_rows[row].add(seat)
+
+        def group_can_seat(r, i, j, res_rows):
+            for i in range(i, j):
+                if i in res_rows[r]:
+                    return False
+                
+            return True
+            
+        count = 0
+        for r in res_rows:
+            fv = group_can_seat(r, 2, 6, res_rows)
+            sv = group_can_seat(r, 4, 8, res_rows)
+            tv = group_can_seat(r, 6, 10, res_rows)
+
+            if fv and sv and tv:
+                count += 2
+            elif fv and tv:
+                count += 2
+            elif fv or sv or tv:
+                count += 1                   
+
+
+        count += (n - len(res_rows)) * 2
+        return count
