372237456b96d6e82f277860dcb0d2d92a95d89d
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-08-20 17:22:20 +0530

    Time: 0 ms (100%), Space: 19.4 MB (53.87%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,62 +1,48 @@
-class Stack:
-    def __init__(self):
-        self.stack = []
-    
-    def push(self, val):
-        self.stack.append(val)
-    
-    def pop(self):
-        if not self.stack:
-            return None
-        
-        return self.stack.pop()
-    
-    def size(self):
-        return len(self.stack)
-
-
-    def isEmpty(self):
-        return len(self.stack) == 0
-    
-
 class MyQueue:
 
     def __init__(self):
-        self.q = Stack()
+        self.stack = []
 
     def push(self, x: int) -> None:
-        self.q.push(x)
+        self.stack.append(x)
 
     def pop(self) -> int:
-        temp = Stack()
+        if not self.stack:
+            return
 
-        while self.q.size() > 1: # till second last element
-            temp.push(self.q.pop())
-        
-        val = self.q.pop() # take the last element 
+        s2 = []
+        n = len(self.stack)
 
-        while temp.size():
-            self.q.push(temp.pop())
+        for _ in range(n - 1):
+            s2.append(self.stack.pop())
         
-        return val
+        x = self.stack.pop()
+        
+        while s2:
+            self.stack.append(s2.pop())
+        
+        return x
 
     def peek(self) -> int:
-        temp = Stack()
+        if not self.stack:
+            return
 
-        while self.q.size() > 1: # till second last element
-            temp.push(self.q.pop())
-        
-        val = self.q.pop() # take the last element 
+        s2 = []
+        n = len(self.stack)
 
-        self.q.push(val)
-        while temp.size():
-            self.q.push(temp.pop())
+        for _ in range(n - 1):
+            s2.append(self.stack.pop())
         
+        x = self.stack.pop()
         
-        return val
+        self.stack.append(x)
+        while s2:
+            self.stack.append(s2.pop())
+            
+        return x
 
     def empty(self) -> bool:
-        return self.q.isEmpty()
+        return len(self.stack) == 0        
 
 
 # Your MyQueue object will be instantiated and called as such:
