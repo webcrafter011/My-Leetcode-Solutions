d999ccad771f02dafe38a1d6e9fcafead7f10468
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-08-24 17:07:56 +0530

    Time: 11 ms (42.42%), Space: 21 MB (48.82%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,39 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
+        pre_max = [None] * n # left max
+        suf_max = [None] * n # right max
+        pre_max[0] = height[0]
+        suf_max[n - 1] = height[n - 1]
 
-        # suf_max = [None] * n
-        # suf_max[-1] = height[-1]
-        # for i in range(n - 2, -1, -1):
-        #     suf_max[i] = max(suf_max[i + 1], height[i])
+        for i in range(1, n):
+            pre_max[i] = max(pre_max[i - 1], height[i])
+
+        for i in range(n - 2, -1, -1):
+            suf_max[i] = max(suf_max[i + 1], height[i])
         
-        # left_max = float('-inf')
-        # trapped_water = 0
-        # for i in range(n):
-        #     left_max = max(left_max, height[i])
-        #     right_max = suf_max[i]
-        #     if height[i] < left_max and height[i] < right_max:
-        #         trapped_water += min(left_max, right_max) - height[i]
-
-        # Optimal solution two pointer 
-        l = 0
-        r = n - 1
-        lmax = rmax = 0
-        trapped_water = 0
-
-        while l < r:
-            if height[l] < height[r]:
-                if lmax > height[l]:
-                    trapped_water += lmax - height[l]
-                else:
-                    lmax = height[l]
-                l += 1
-            else:
-                if rmax > height[r]:
-                    trapped_water += rmax - height[r]
-                else:
-                    rmax = height[r]
-                r -= 1
-            
+        water = 0
+        for i in range(n):
+            if height[i] < pre_max[i] and height[i] < suf_max[i]:
+                water += min(pre_max[i], suf_max[i]) - height[i]
         
-        return trapped_water
+        return water
