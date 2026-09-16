9530a174fa8110848daaa577fbba4b313dc4b606
Manuj Chaudhari <153096421+webcrafter011@users.noreply.github.com>
2026-09-16 18:46:43 +0530

    Time: 33 ms (57.77%), Space: 20 MB (92.7%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,22 +1,28 @@
 class Solution:
-    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        def at_most(target):
-            count = 0
-            left = 0
+    def numSubarraysWithSum(self, nums: list[int], goal: int) -> int:
+        
+        def get_at_most(target):
+            # this function returns the number of subarrays that sum up to at most target
+
+            if target < 0:
+                return 0
+
             res = 0
+            left = 0
+            count = 0
 
             for right in range(len(nums)):
-                if target < 0:
-                    return 0
-                    
                 count += nums[right]
 
                 while count > target:
                     count -= nums[left]
                     left += 1
                 
-                res += (right - left + 1)
+                res += right - left + 1
             
             return res
         
-        return at_most(goal) - at_most(goal - 1)
+
+        # we will return the number of subarrays that sum up to target - number of subarrays that sum up to (target - 1) in result we end up with the exact number of subarray that sum up to exactly target
+        
+        return get_at_most(goal) - get_at_most(goal - 1)
